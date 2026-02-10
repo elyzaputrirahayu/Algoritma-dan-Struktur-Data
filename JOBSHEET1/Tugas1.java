c662d54d1cbbe6ede715bbd0be01b83935380721
elyzaputrirahayu <eliscantik2000@gmail.com>
2026-02-10 10:44:41 +0700

    Tugas1

---
+++ JOBSHEET1/Tugas1.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Tugas1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[] KODE = {'A','B','D','E','F','G','H','L','N','T'};
+
+        // Array 2 dimensi: nama kota (per huruf)
+        char[][] KOTA = {
+            {'B','A','N','D','U','N','G'},
+            {'J','A','K','A','R','T','A'},
+            {'B','A','N','D','U','N','G'},
+            {'C','I','R','E','B','O','N'},
+            {'B','O','G','O','R'},
+            {'P','E','K','A','L','O','N','G','A','N'},
+            {'S','E','M','A','R','A','N','G'},
+            {'S','U','R','A','B','A','Y','A'},
+            {'M','A','L','A','N','G'},
+            {'T','E','G','A','L'}
+        };
+
+        System.out.print("Masukkan kode plat nomor: ");
+        char inputKode = sc.next().charAt(0);
+
+        boolean ditemukan = false;
+
+        // proses pencarian
+        for (int i = 0; i < KODE.length; i++) {
+            if (inputKode == KODE[i]) {
+                System.out.print("Nama Kota: ");
+                for (int j = 0; j < KOTA[i].length; j++) {
+                    System.out.print(KOTA[i][j]);
+                }
+                System.out.println();
+                ditemukan = true;
+                break;
+            }
+        }
+
+        if (!ditemukan) {
+            System.out.println("Kode plat tidak ditemukan");
+        }
+
+        sc.close();
+    }
+}
