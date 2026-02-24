d329ae79349064566a88ec3e34df7a0d0f91e311
elyzaputrirahayu <eliscantik2000@gmail.com>
2026-02-24 08:57:06 +0700

    Percobaan 3

--- JOBSHEET2/Mahasiswa08.java
+++ JOBSHEET2/Mahasiswa08.java
@@ -6,6 +6,17 @@ public class Mahasiswa08 {
     String kelas;
     double ipk;
 
+     public Mahasiswa08() {
+
+        }
+
+        public Mahasiswa08(String nm, String nim, double ipk, String kls) {
+            nama = nm;
+            this.nim = nim;
+            this.ipk = ipk;
+            kelas = kls;
+        }
+
     void tampilkanInformasi () {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
--- JOBSHEET2/MahasiswaMain08.java
+++ JOBSHEET2/MahasiswaMain08.java
@@ -12,5 +12,9 @@ public class MahasiswaMain08 {
         mhs1.ubahkelas("SI 2K");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa08 mhs2 = new Mahasiswa08("Annisa Nabila", "2141720160", 3.25, "Ti 2l");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
