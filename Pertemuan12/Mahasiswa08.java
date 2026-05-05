5fac7337514d8187fbb12f08ea48d0766e9b1bde
elyzaputrirahayu <eliscantik2000@gmail.com>
2026-05-05 08:08:17 +0700

    Percobaan 1

---
+++ Pertemuan12/Mahasiswa08.java
@@ -0,0 +1,25 @@
+package Pertemuan12;
+
+import org.w3c.dom.Node;
+
+public class Mahasiswa08 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    Mahasiswa08() {
+
+    }
+
+    Mahasiswa08(String nm, String name, String kls, double ip) {
+        nama = name;
+        nim = nm;
+        kelas = kls;
+        ipk = ip;
+    }
+
+    void tampilInformasi() {
+        System.out.println(nama + "\t " + nim + "\t" + kelas + "\t" + ipk);
+    }
+}
