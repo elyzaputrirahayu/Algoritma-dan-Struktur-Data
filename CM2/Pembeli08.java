1ee3f79f5f659e9915b7f1bf5b1e3e1cb0012822
elyzaputrirahayu <eliscantik2000@gmail.com>
2026-05-12 11:47:26 +0700

    LAPORAN PROGRES CM 2

---
+++ CM2/Pembeli08.java
@@ -0,0 +1,11 @@
+package CM2;
+
+public class Pembeli08 {
+    String namaPembeli;
+    String NoHp;
+
+    public Pembeli08 (String b, String c) {
+        this.namaPembeli = b;
+        this.NoHp = c;
+    }
+}
---
+++ CM2/Pesanan08.java
@@ -0,0 +1,13 @@
+package CM2;
+
+public class Pesanan08 {
+    int kodePesanan;
+    String namaPesanan;
+    int harga;
+
+    public Pesanan08 (int a, String b, int d) {
+        this.kodePesanan = a;
+        this.namaPesanan = b;
+        this.harga = d;
+    }
+}
