34867354cabdfee3a6fe15f372d5fdbb5cb9e9ff
elyzaputrirahayu <eliscantik2000@gmail.com>
2026-05-26 09:05:05 +0700

    Percobaan 1 sama 2

---
+++ PERTEMUAN14/Node08.java
@@ -0,0 +1,15 @@
+package PERTEMUAN14;
+
+public class Node08 {
+    Mahasiswa08 mahasiswa;
+    Node08 left, right;
+
+    public Node08() {
+
+    }
+
+    public Node08(Mahasiswa08 mahasiswa) {
+        this.mahasiswa = mahasiswa;
+        left = right = null;
+    }
+}
